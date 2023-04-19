a915b23ba5330690f94d4ab15adebc621352a09a
mgratz@frba.utn.edu.ar <mgratz@frba.utn.edu.ar>
2023-04-19 18:02:18 -0300

    notas y completo clase Prenda

--- src/main/java/Atuendo.java
+++ src/main/java/Atuendo.java
@@ -1,5 +1,3 @@
-// Creo clase atuendo como fabrica de Atuendos
-// Como usuario de QuéMePongo, quiero poder cargar prendas válidas para generar atuendos con ellas.
 
 public class Atuendo {
 
--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -1,3 +1,11 @@
 public class Prenda {
+  public  var tipo // (zapatos, camisa de mangas cortas, pantalón, etc).
+      public var categoria // (parte superior, calzado, parte inferior, accesorios).
+  publica var material
+  public var colorPrincipal
+      publicar var colorSecundario
+
+
 
 }
+
