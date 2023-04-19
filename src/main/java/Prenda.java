9133d805abc477501ed3ce4441a1517e2721c75a
mgratz@frba.utn.edu.ar <mgratz@frba.utn.edu.ar>
2023-04-19 18:39:48 -0300

    Conexion entre Prenda y Atuendo

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -1,5 +1,5 @@
 public class Prenda {
-  public  var tipo // (zapatos, camisa de mangas cortas, pantalón, etc).
+  public  var tipo; // (zapatos, camisa de mangas cortas, pantalón, etc).
       public var categoria // (parte superior, calzado, parte inferior, accesorios).
   publica var material
   public var colorPrincipal
