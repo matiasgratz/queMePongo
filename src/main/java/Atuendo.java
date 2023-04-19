c3ed2cb4fa4e86e7a9012d2e6bd59a7e9d4cf38c
mgratz@frba.utn.edu.ar <mgratz@frba.utn.edu.ar>
2023-04-19 17:48:52 -0300

    Agrego Clases Tentativas

---
+++ src/main/java/Atuendo.java
@@ -0,0 +1,8 @@
+// Creo clase atuendo como fabrica de Atuendos
+// Como usuario de QuéMePongo, quiero poder cargar prendas válidas para generar atuendos con ellas.
+
+public class Atuendo {
+
+}
+
+
