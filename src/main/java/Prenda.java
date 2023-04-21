54c46408c465b0cabc43115443a2af262fb57687
mgratz@frba.utn.edu.ar <mgratz@frba.utn.edu.ar>
2023-04-21 18:03:33 -0300

    Lanzando Excepciones

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -6,8 +6,28 @@ public class Prenda {
   String colorSecundario;
 
   public Prenda(String tipo, Categoria categoria, String material, String colorPrincipal, String colorSecundario){
+    this.tipo = tipo;
+    this.categoria = categoria;
+    this.material = material;
+    this.colorPrincipal = colorPrincipal;
+    this.colorSecundario = colorSecundario;
+    if (tipo == null){
+        throw new RuntimeException("La prenda debe tener un tipo");
+    }
+    if (material == null){
+        throw new RuntimeException("La prenda debe tener un material");
+    }
+    if (categoria == null){
+        throw new RuntimeException("La prenda debe tener un categoria");
+    }
+    if (colorPrincipal == null){
+        throw new RuntimeException("La prenda debe tener un color principal");
+      }
+
 
   }
+
+
 
   // Como usuarie de QuéMePongo, quiero evitar que haya prendas sin tipo, tela, categoría o color primario.
     // Como usuarie de QuéMePongo, quiero evitar que haya prendas cuya categoría no se condiga con su tipo.
